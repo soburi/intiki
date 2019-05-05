bc012ddef610f7a3f070de02c58b5105aaf30c4c
soburi <soburi@gmail.com>
2019-05-05 23:08:52 +0900

    Output syslog

--- DebugLog.go
+++ DebugLog.go
@@ -1,7 +1,13 @@
 // +build !windows
 
 package main
-
+import "log/syslog"
 
 func DebugLog(msg string) {
+	logger, err := syslog.New(syslog.LOG_DEBUG, "intiki");
+
+	if err == nil {
+		logger.Notice(msg)
+	}
 }
+
